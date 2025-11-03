b7593e15ccf7f4a35a856737d84a442326539a66
Tadas <tadovas@gmail.com>
2025-11-03 16:05:09 +0200

    Dockerized

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# ------ Builder Stage ------
+FROM oven/bun:1.1.35-alpine AS builder
+WORKDIR /app
+
+COPY package.json bun.lockb* ./
+RUN bun install --frozen-lockfile
+
+COPY src ./src
+COPY tsconfig.json ./
+
+RUN bun run tsc
+
+# ------ Runner Stage ------
+FROM oven/bun:1.1.35-alpine AS runner
+WORKDIR /app
+
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/dist ./dist
+COPY --from=builder /app/package.json ./
+
+USER bun
+EXPOSE 3000
+
+CMD ["node", "dist/app.js"]
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,26 @@
-console.log("hello, boss")
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.json())
+
+app.all(/.*/, (req, res) => {
+
+    console.log('Headers: ', jsonify(req.headers))
+    console.log('Body: ', jsonify(req.body))
+
+
+    res.status(503).send({
+        'error': 'Not implemented!'
+    })
+});
+
+app.listen(port, () => {
+    console.log(`🚀 Server running on http://localhost:${port}`);
+});
+
+function jsonify(obj: any): string {
+    return JSON.stringify(obj, undefined, 2)
+}
